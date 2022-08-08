8d4eff04d27bd270b1025729240afc2efca7ae75
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-08-08 21:25:24 +0200

    instsll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+// form.addEventListener('input', throttle(onTextareaInput, 500));
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notify.success('Sol lucet omnibus');
+// Notify.failure('Qui timide rogat docet negare');
+// Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+
+
 
 const DEBOUNCE_DELAY = 300;
