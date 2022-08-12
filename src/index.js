1137bff0a0e22902bcb1a7e886be762739b549b9
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-08-13 00:53:41 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-// form.addEventListener('input', throttle(onTextareaInput, 500));
+import API from './js/fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+//import Notiflix { Notify } from 'notiflix';
+
 
 // Notify.success('Sol lucet omnibus');
 // Notify.failure('Qui timide rogat docet negare');
@@ -13,41 +15,62 @@ const countryInfo = document.querySelector('.country-info');
 
 console.log(searchBox);
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
     e.preventDefault();
-    const searchQuery = searchBox.value;
+    const searchQuery = searchBox.value.trim(); 
     console.log(searchQuery);
 
-    fetchCountries(searchQuery);//.then(renderCountry);
+    if (searchQuery === "") {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+        return;
+    } 
 
+    API.fetchCountries(searchQuery)
+        .then(renderCountry)
+        .catch(error => Notify.failure("Oops, there is no country with that name") 
+         );
+} 
+
+function renderCountry(item) {
+    const countContries = item.length;
+    console.log(item.length);
+    
+    if (countContries === 1) {
+        dataofCountry(item);
+        countryList.innerHTML = '';
+
+    } else if (countContries > 1 && countContries <= 10) {
+        listOfCountries(item);
+        countryInfo.innerHTML = '';  
+    } else if (countContries > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+    };
 }
 
-const BASE_URL = "https://restcountries.com/v2/name/";
-function fetchCountries(name) {
-// const r =
-   return fetch(`BASE_URL${name}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            // console.log(response.json);
-            return response.json();
-        })
-       .then(country => {
-           console.log("data", country);
-          const stroka = `<li>"${ Object.values(name) }"</li>`//`<li>${Object.values(capital)}</li>`;
-          console.log(stroka);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-// console.log(r);
-
+function dataofCountry(item) {
+    const markupData = item.map(({name, capital, population, flags, languages}) => {
+        return `<h2> <img src="${flags.svg}" alt="${name.official}" width=70px>
+            ${name.official} </h2>
+             <p class="text"> Capital: ${capital} </p>
+            <p class="text">Population: ${population}</p>
+            <p class="text">Languages: ${Object.values(languages).join(', ')}</p>`
+    }).join("");
+    countryInfo.innerHTML = markupData;
+    
 }
 
-// function renderCountry(item) {
-//     const stroka = "<li>${item.name}</li>";
-//     console.log(stroka);
+function listOfCountries(item) {
+    const markupList = item.map(({ name, flags }) => {
+        return `<li class="text"> <img src="${flags.svg}" alt="${name.official}" class="img-list">
+        ${name.official}</li>`
+    }).join('');
 
-// }
+    countryList.innerHTML = markupList;
+}
